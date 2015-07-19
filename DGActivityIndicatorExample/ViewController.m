3f334c37fbda799967c068eaa60b0dc7994d0f94
Nguyen Vinh <ninjaprox@gmail.com>
2015-07-20 00:13:28 +0400

    Ball clip rotate multiple demo

--- DGActivityIndicatorExample/ViewController.m
+++ DGActivityIndicatorExample/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
     self.view.backgroundColor = [UIColor colorWithRed:237/255.0f green:85/255.0f blue:101/255.0f alpha:1.0f];
     
     NSArray *activityTypes = @[@(DGActivityIndicatorAnimationTypeNineDots), @(DGActivityIndicatorAnimationTypeTriplePulse),
@@ -25,12 +25,14 @@
                                @(DGActivityIndicatorAnimationTypeThreeDots),
                                @(DGActivityIndicatorAnimationTypeBallPulse),
                                @(DGActivityIndicatorAnimationTypeBallClipRotate),
-                               @(DGActivityIndicatorAnimationTypeBallClipRotatePulse)];
+                               @(DGActivityIndicatorAnimationTypeBallClipRotatePulse),
+                               @(DGActivityIndicatorAnimationTypeBallClipRotateMultiple)];
     NSArray *sizes = @[@(40.0f), @(40.0f),
                        @(35.0f), @(40.0f),
                        @(40.0f), @(20.0f),
                        @(40.0f), @(40.0f),
-                       @(40.0f), @(40.0f)];
+                       @(40.0f), @(40.0f),
+                       @(40.0f)];
     for (int i = 0; i < activityTypes.count; i++) {
         DGActivityIndicatorView *activityIndicatorView = [[DGActivityIndicatorView alloc] initWithType:(DGActivityIndicatorAnimationType)[activityTypes[i] integerValue] tintColor:[UIColor whiteColor] size:[sizes[i] floatValue]];
         
