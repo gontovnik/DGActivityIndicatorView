2ca4ba0bdc50f87e24223c3e4a3ebe75b29a7fd9
Rafael Fernández <rafael.fernandez@sdos.es>
2021-05-18 08:35:10 +0200

    change directories for spm error

--- Package.swift
+++ Package.swift
@@ -18,10 +18,7 @@ let package = Package(
         .target(
             name: "DGActivityIndicatorView",
             path: "DGActivityIndicatorView",
-            publicHeadersPath: "",
-            cSettings: [
-                .headerSearchPath("Animations"),
-                  ]
+            publicHeadersPath: ""
         ),
     ]
 )
